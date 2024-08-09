62ad012fdf79fc73b0ad57ba79c454d64220145b
Brad Dougherty <brad.doughertys@gmail.com>
2024-08-09 11:44:43 +0200

    Added comments to everything

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # Score-More ReadMe
 
 ## What is Score-More?
-Score-More is a live scoreboard app that lets each individual in a room keep their score when pen & paper isn't an option, and keeps a group from needing 1 dedicated score keeper.
+Score-More is a live scoreboard app that has each individual keep their score when pen & paper isn't an option, and keeps a group from needing a single dedicated score keeper.
 Creating a room generates a 4 letter room code (no vowels) and sends you to that room. People can join using the room code on the home page.
 
 
 ## What did I use?
 Runs on Node.js using express.
-Handles users with socket.io and redis
+Handles users with socket.io and stores player data with Redis lists and sorted sets.
 
 ---
 
 
--v1.0
+### 12 June 2024
 Initial version that just has a live score board. The home page lets you create or join a room and has each player appear on the board with their total score. Players are ordered by the order in which they join.
 
--Wishlist to add\
-Sort players by high or low score.\
-Be able to change name from Player-(n) to a custom name.\
-View other players score history.
+---
 
-Feel free to contact me.
+### 4 July 2024
+Updated Redis data types to lists for score history and sorted sets for totals.\
+Users can now enter a name.\
+Users can now sort scores by ascending or descending order.\
+Users can now view the score history of any player in the room.
+
+---
+
+### 9 July 2024
+Fixed issues when user has a space or special characters in their name.\
+Room code field is now case insensitive.\
+Entering an invalid room code now correctly shows an error page.
+
+---
+
+### 9 August 2024
+No functionality changes. Added comments to everything so future me isn't mad and confused.\
+
+---
--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const Redis = require('redis');
 const redisClient = Redis.createClient({url: process.env.REDISCLOUD_URL, tls: {rejectUnauthorized: false}})
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const { render } = require('ejs');
 
+//
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -16,7 +16,7 @@ const io = new Server(server, {
   }
 })
 
-// redis initialization
+// redis initialization, with a confirmation or errors on startup
 redisClient.connect()
 redisClient.on('connect', () => {
   console.log('Redis client connected');
@@ -25,24 +25,36 @@ redisClient.on('error', (err) => {
   console.log('Redis error:', err)
 })
 
+// Cors and body parser let us handle the info coming from the client.
 app.use (cors());
 app.use(bodyParser.json());
+// Using ejs initially because I thought I would have more embedded JS in the body
+// but I think we have basically 0...not gonna change it unless i have to though
 app.set('view engine', 'ejs');
+// Uses the public directory to render the CSS for the pages
 app.use(express.static('public'));
 
-// available letters for room generation
+// available letters for room generation, no vowels to avoid "naughty" words
 const LETTERS = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
-// an array of the active room codes.
+// An array of the active room codes that have been generated. Used to help validate room codes.
+// This may be memory inefficient at scale, since arrays are "memory intensive". That is a cross
+// that bridge if I ever come to it. #Doubt
 let activeRooms = [];
 
+// Express server endpoints. Root path here is for the homepage "index.ejs"
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Error page if the client messes up the room code. Pseudo 404 page.
 app.get('/error', (req, res) => {
   res.render('error');
 });
 
+// Post endpoint that accepts the "index.ejs" form data. Takes the payer name and
+// if given the room code. If a code is given it validates it is "active", if it is
+// we add the player to the sorted set and instantiate them with a 0 score. If it isnt
+// we direct them to the error page.
 app.post('/add-player', (req, res) => {
   let playerName = req.body.playerName
   let roomCode = ""
@@ -69,13 +81,18 @@ app.post('/add-player', (req, res) => {
   redisClient.expire(roomCode, 7200)
   res.json({ playerName, roomCode })
 })
-
+// url for the room with the player's name
 app.get("/room/:room/:name", (req, res) => {
   res.render('room');
   });
 
-
+// socket io events
 io.on('connection', socket => {
+
+  // When a client connects, they emit a join room event that puts them in
+  // a room named by the roomcode. This emits an update board action to that room
+  // so that they show up on everyones board. We get the sorted set of the room
+  // from Redis in ascending order and return it to the room with the update-board event
   socket.on('join-room', (roomCode) => {
     socket.join(roomCode)
     redisClient.zRangeWithScores(roomCode, 0, -1).then((players) => {
@@ -83,6 +100,11 @@ io.on('connection', socket => {
     })
   })
 
+  // When a client enters a score this they emit the update score action, this
+  // sends the roomcode, who the player is and the integer value of the score
+  // We do a 'right push' on their list of scores, increment their total
+  // in the hash, and return the updated sorted set in ascending order to the room
+  // everytime a score is updated their expire time is reset
   socket.on('update-score', (roomCode, playerName, score) => {
     redisClient.rPush(roomCode + playerName, score)
     redisClient.zIncrBy(roomCode, score, playerName)
@@ -92,18 +114,31 @@ io.on('connection', socket => {
     redisClient.expire(roomCode + playerName, 7200)
   })
 
+  // At the end of a update board event on the client side, there is then a request
+  // for the history of the currently selected player.
   socket.on('get-history', (roomCode, playerName) => {
     redisClient.lRange(roomCode + playerName, 0, -1).then((history) => {
       socket.emit('send-history', history)
     })
   })
 
+  // When the client changes the value of the sort-by selection, respond with
+  // the sorted set of the room.
+  // TODO: Would it be better to update this to respond to only the socket that
+  // made the request?
   socket.on('update-order', (roomCode) => {
     redisClient.zRangeWithScores(roomCode, 0, -1).then((players) => {
       io.to(roomCode).emit('update-board', players)
     })
   })
 
+  // When a client begins disconnecting we use the handshake content to remove
+  // that player from the sorted set, and delete the list of their scores.
+  // Then an update board event is sent to change the rosters and history selections of
+  // the clients in that room.
+  // TODO: Some issues on mobile timing out and being seen as disconnecting when the phone
+  // sleeps. Is there a better way to handle disconnects? Or should we just not handle them
+  // and let them time out, and not update boards on a player leaving.
   socket.on('disconnecting', () => {
     socket.handshake.headers.playername || socket.handshake.headers.roomcode
     player = socket.handshake.headers.playername
@@ -116,7 +151,8 @@ io.on('connection', socket => {
   })
 })
 
-// generates a random 4 letter room code
+// generates a random 4 letter room code. LETTERS has vowels excluded to avoid
+// the "naughty" room codes. I dont want to mess with the logic of blacklisting words
 function generateRoom() {
   let room = "";
   for (let i = 0; i < 4; i++) {
@@ -126,7 +162,7 @@ function generateRoom() {
   return room;
 }
 
-// starts the server on port 3000
+// starts the server on the environment port or 3000 in development.
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
